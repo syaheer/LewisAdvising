1a1e85b24c32181a8c83cfdb9362ebda79faa707
Sazali, Syahir Amir <Syahir.Sazali@chamberlain.com>
2019-10-17 00:24:09 -0500

    add password reset feature

--- app/src/main/java/com/groupb/lewisadvising/MainActivity.java
+++ app/src/main/java/com/groupb/lewisadvising/MainActivity.java
@@ -1,13 +1,16 @@
 package com.groupb.lewisadvising;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,16 +18,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         final TextView email = findViewById(R.id.email);
         final TextView password = findViewById(R.id.password);
         Button loginButton = findViewById(R.id.loginButton);
+        TextView forgotPassword = findViewById(R.id.forgot_password);
         mAuth = FirebaseAuth.getInstance();
 
         signUpText.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +60,49 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             }
                         });
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final View view1 = getLayoutInflater().inflate(R.layout.dialog_forgot_password, null);
+                final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Reset Password");
+                alertDialog.setCancelable(false);
+                alertDialog.setMessage("Enter your email address");
+
+
+                final EditText etComments = view1.findViewById(R.id.etComments);
+
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (!etComments.getText().toString().isEmpty())
+                            mAuth.sendPasswordResetEmail(etComments.getText().toString())
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(MainActivity.this, "Password reset email sent.",
+                                                        Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                    }
+                });
+
+
+                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+
+                alertDialog.setView(view1);
+                alertDialog.show();
             }
         });
     }
